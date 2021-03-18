a5f2404d583ddd033332443728fc04353d0bf163
Elayamani K <k.elayamani@gmail.com>
2021-03-18 01:03:06 -0700

    fixes -- keep things simple

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
+const production = !process.env.ROLLUP_WATCH;
 module.exports = {
   purge: {
     content: [
       "./src/**/*.svelte",
     ],
-    enabled: process.env.NODE_ENV === 'production' // disable purge in dev
+    enabled: production
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
