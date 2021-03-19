645a6c64182e64b81b8a6d235c70c7957128d8b5
Elayamani K <k.elayamani@gmail.com>
2021-03-19 16:00:48 -0700

    prettify

---
+++ src/components/DateValue.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { formatCurrency } from "utils";
+
+  export let date: string;
+  export let value: number;
+</script>
+
+<div class="flex w-full justify-between text-gray-600">
+  <div>{date} PnL</div>
+  <div>:</div>
+  <div class="text-left font-medium">
+    {value >= 0 ? "" : "-"}${formatCurrency(Math.abs(value), 2)}
+  </div>
+</div>
