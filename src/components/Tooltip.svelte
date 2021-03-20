98b0dabca056a7aa88ba8ac0679fb1d6b0dad973
Elayamani K <k.elayamani@gmail.com>
2021-03-20 02:16:46 -0700

    add tooltip to avoid confusion

---
+++ src/components/Tooltip.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+  export let tooltip: string;
+</script>
+
+<div class="flex w-full" data-tooltip={tooltip}>
+  <div class="flex flex-grow items-center space-x-2">
+    <slot />
+    <svg
+      class="cursor-pointer"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      width="1em"
+      height="1em"
+      fill="#6b7280"
+    >
+      <path
+        d="M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zM262.655 90c-54.497 0-89.255 22.957-116.549 63.758-3.536 5.286-2.353 12.415 2.715 16.258l34.699 26.31c5.205 3.947 12.621 3.008 16.665-2.122 17.864-22.658 30.113-35.797 57.303-35.797 20.429 0 45.698 13.148 45.698 32.958 0 14.976-12.363 22.667-32.534 33.976C247.128 238.528 216 254.941 216 296v4c0 6.627 5.373 12 12 12h56c6.627 0 12-5.373 12-12v-1.333c0-28.462 83.186-29.647 83.186-106.667 0-58.002-60.165-102-116.531-102zM256 338c-25.365 0-46 20.635-46 46 0 25.364 20.635 46 46 46s46-20.636 46-46c0-25.365-20.635-46-46-46z"
+      />
+    </svg>
+  </div>
+</div>
+
+<svelte:head>
+  <style>
+    /*This would all go into the global.css file*/
+    [data-tooltip] {
+      position: relative;
+      z-index: 2;
+      display: block;
+    }
+
+    [data-tooltip]:before,
+    [data-tooltip]:after {
+      visibility: hidden;
+      opacity: 0;
+      pointer-events: none;
+      transition: 0.2s ease-out;
+      transform: translate(-50%, 5px);
+    }
+
+    [data-tooltip]:before {
+      position: absolute;
+      bottom: 100%;
+      left: 50%;
+      margin-bottom: 5px;
+      padding: 7px;
+      width: 100%;
+      min-width: 70px;
+      max-width: 250px;
+      -webkit-border-radius: 3px;
+      -moz-border-radius: 3px;
+      border-radius: 3px;
+      background-color: #000;
+      background-color: hsla(0, 0%, 20%, 0.9);
+      color: #fff;
+      content: attr(data-tooltip);
+      text-align: center;
+      font-size: 14px;
+      line-height: 1.2;
+      transition: 0.2s ease-out;
+    }
+
+    [data-tooltip]:after {
+      position: absolute;
+      bottom: 100%;
+      left: 50%;
+      width: 0;
+      border-top: 5px solid #000;
+      border-top: 5px solid hsla(0, 0%, 20%, 0.9);
+      border-right: 5px solid transparent;
+      border-left: 5px solid transparent;
+      content: " ";
+      font-size: 0;
+      line-height: 0;
+    }
+
+    [data-tooltip]:hover:before,
+    [data-tooltip]:hover:after {
+      visibility: visible;
+      opacity: 1;
+      transform: translate(-50%, 0);
+    }
+    [data-tooltip="false"]:hover:before,
+    [data-tooltip="false"]:hover:after {
+      visibility: hidden;
+      opacity: 0;
+    }
+  </style>
+</svelte:head>
